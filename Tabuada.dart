97a4af873c9fe4280aa5913cbb95011cc818d8ff
Danilo Herdy Oliveira <daniloho89@gmail.com>
2020-05-15 06:51:13 -0300

    Parte 1 - Aula 05
    O laço de repetição while.
    O laço de repetição do/while.
    O laço de repetição for.
    Quando utilizamos o break.
    As diferenças entre os laços de repetição.

---
+++ Tabuada.dart
@@ -0,0 +1,9 @@
+void main() {
+  for (int j = 1; j <= 10; j++) {
+    
+    print("Tabuada de $j");
+    for (int i = 1; i <= 10; i++) {
+      print("$j * $i = ${j * i}");
+    }
+  }
+}
---
+++ testarBreak.dart
@@ -0,0 +1,11 @@
+void main() {
+  int contador = 0;
+
+  for(;;) {
+    contador++;
+    print("valor do contador é $contador");
+    if(contador==123)
+      break;
+  }
+
+}
