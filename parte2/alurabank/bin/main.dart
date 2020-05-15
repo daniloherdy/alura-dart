217288b476bca58f3e3dd80ea93b8dd791d8f957
Danilo Herdy Oliveira <daniloho89@gmail.com>
2020-05-15 07:26:05 -0300

    Parte 2 - Aula 02
    Como acessar e alterar campos de um objeto;
    Valores padrão de campos de classes;
    Tipos de referências;
    Inicialização de campos

--- parte2/alurabank/bin/main.dart
+++ parte2/alurabank/bin/main.dart
@@ -1,13 +1,17 @@
 void main() {
-  ContaCorrente conta;
-  
-  
+  ContaCorrente conta = ContaCorrente();
+  conta.titular = 'Danilo';
+  conta.agencia = 012;
+  conta.conta = 123;
+  conta.saldo = 150.0;
 
+
+  print(conta.saldo);
 }
 
 class ContaCorrente {
   String titular;
-  int agencia;
+  int agencia = 145;
   int conta;
-  double saldo;
+  double saldo = 0.0;
 }
