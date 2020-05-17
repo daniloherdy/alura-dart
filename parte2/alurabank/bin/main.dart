b5339cddb240640a8ae9119e11e711bdf531714d
Danilo Herdy Oliveira <daniloho89@gmail.com>
2020-05-17 15:08:48 -0300

    Parte 2 - Aula 04
    Ajustando a instancia da main

--- parte2/alurabank/bin/main.dart
+++ parte2/alurabank/bin/main.dart
@@ -1,19 +1,16 @@
 import '../lib/cliente.dart';
 import '../lib/contaCorrente.dart';
+
 void main() {
   ContaCorrente conta = ContaCorrente();
-  Cliente cliente1 = new Cliente();
-
-  cliente1.nome = 'Danilo';
-  cliente1.cpf = '123.456.789-00';
-  cliente1.profissao = 'A';
+  
+  Cliente cliente1 =  Cliente()
+  ..nome = 'Danilo'
+  ..cpf = '123.456.789-00'
+  ..profissao = 'A';
 
   conta.titular = cliente1;
   conta.saque(200.0);
 
   print('Saldo de ${conta.titular}: ${conta.saldo}');
 }
-
-
-
-
